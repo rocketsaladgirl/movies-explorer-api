0162c233dc0b9f97937e1a920f2c43ed9aa470e8
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-07-14 16:52:35 +0300

    Исправлен app для взаимодействия с frontend-частью

--- app.js
+++ app.js
@@ -2,46 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const { errors } = require('celebrate');
 const cors = require('cors');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cookieParser = require('cookie-parser');
 const limiter = require('./middlewares/rateLimit');
 
 const router = require('./routes/index');
 
 const handleError = require('./middlewares/handelError');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { baseUrl } = require('./utils/option');
+const { requestLogger } = require('./middlewares/logger');
 
-const { NODE_ENV, MONGO_URL } = process.env;
+const { corsOptions } = require('./utils/constants');
+
+const { MONGO_URL } = process.env;
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
-app.use(helmet());
-
 app.use(express.json());
 
+mongoose.connect(MONGO_URL, {});
+
 app.use(requestLogger);
+app.use(cookieParser());
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(limiter);
 
-app.use(router);
+app.use('/', router);
 
-app.use(errorLogger);
-app.use(errors());
 app.use(handleError);
 
-async function start() {
-  try {
-    await mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : baseUrl);
-    await app.listen(PORT);
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-  }
-}
-
-start()
-  // eslint-disable-next-line no-console
-  .then(() => console.log(`Приложение успешно запущенно!\n${MONGO_URL}\nPort: ${PORT}`));
+app.listen(PORT, () => {});
