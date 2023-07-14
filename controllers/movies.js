36f32e2c84b134e71cd49ddef15129c19c290e22
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-07-14 16:20:02 +0300

    Добавлена функция деавторизации пользователя, скорректирован controllers/movies для взаимодействия с frontend-частью

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,92 +1,60 @@
 const movieSchema = require('../models/movie');
 
-const BadRequestError = require('../errors/BadRequestError');
-const NotFoundError = require('../errors/NotFoundError');
-const ForbiddenError = require('../errors/ForbiddenError');
+const BadRequestError = require('../errors/BadRequestError'); // 400
+const NotFoundError = require('../errors/NotFoundError'); // 404
+const ForbiddenError = require('../errors/ForbiddenError'); // 403
 
 const {
   NOT_FOUND_MOVIE,
-  WRONG_MOVIE,
   ACCESS_ERROR,
   DELETE_WRONG_MOVIE,
 } = require('../utils/constants');
 
 // Возвращаем сохранённые текущим пользователем фильмы
 module.exports.getMovies = (req, res, next) => {
-  movieSchema.find({ owner: req.user._id })
+  const currentUserId = req.user._id;
+
+  movieSchema.find({ owner: currentUserId })
     .then((movies) => {
-      res.send(movies);
+      res.status(200).send(movies);
     })
     .catch(next);
 };
 
 // Создаем фильм
 module.exports.createMovie = (req, res, next) => {
-  const {
-    country,
-    director,
-    duration,
-    year,
-    description,
-    image,
-    trailerLink,
-    thumbnail,
-    movieId,
-    nameRU,
-    nameEN,
-  } = req.body;
+  const currentUserId = req.user._id;
 
-  movieSchema
-    .create({
-      country,
-      director,
-      duration,
-      year,
-      description,
-      image,
-      trailerLink,
-      thumbnail,
-      movieId,
-      nameRU,
-      nameEN,
-      owner: req.user._id,
-    })
-    .then((movie) => res.send(movie))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(
-          new BadRequestError(WRONG_MOVIE), // Переданы некорректные данные при создании фильма.
-        );
-      } else {
-        next(err);
-      }
-    });
+  movieSchema.create({ ...req.body, owner: currentUserId })
+    .then((movie) => res.status(201).send(movie))
+
+    .catch(next);
 };
 
 // Удаляем фильм
 module.exports.deleteMovie = (req, res, next) => {
-  movieSchema.findById(req.params.movieId)
-    .orFail(() => {
-      throw new NotFoundError(NOT_FOUND_MOVIE); // Фильм с указанным _id не найден.
-    })
+  const currentUserId = req.user._id;
+
+  movieSchema.findById(req.params._id)
+    .orFail()
     .then((movie) => {
-      const owner = movie.owner.toString();
-      if (req.user._id === owner) {
-        movieSchema.deleteOne(movie)
-          .then(() => {
-            res.send(movie);
-          })
-          .catch(next);
-      } else {
+      const ownerId = movie.owner.toString();
+      if (ownerId !== currentUserId) {
         // eslint-disable-next-line max-len
         throw new ForbiddenError(ACCESS_ERROR); // Отказано в доступе! Данный фильм не принадлежит пользователю!
       }
+      return movie;
     })
+    .then((movie) => movie.deleteOne())
+    .then((movie) => res.status(200).send(movie))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequestError(DELETE_WRONG_MOVIE)); // Переданы некорректные данные при удалении!
-      } else {
-        next(err);
+      if (err.name === 'DocumentNotFoundError') {
+        return next(new NotFoundError(NOT_FOUND_MOVIE)); // Фильм с указанным _id не найден.
       }
+      if (err.name === 'CastError') {
+        // eslint-disable-next-line max-len
+        return next(new BadRequestError(DELETE_WRONG_MOVIE)); // Переданы некорректные данные при удалении!
+      }
+      return next(err);
     });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -4,40 +4,41 @@ const jwt = require('jsonwebtoken');
 
 const userSchema = require('../models/user');
 
-const BadRequestError = require('../errors/BadRequestError');
-const NotFoundError = require('../errors/NotFoundError');
-const ConflictError = require('../errors/ConflictError');
+const BadRequestError = require('../errors/BadRequestError'); // 400
+const NotFoundError = require('../errors/NotFoundError'); // 404
+const ConflictError = require('../errors/ConflictError'); // 403
+const NotAuthError = require('../errors/NotAuthError'); // 401
 
 const {
   EMAIL_ALREADY_EXISTS,
-  WRONG_UPDATE_PROFILE,
+  WRONG_EMAIL_OR_PASSWORD,
   WRONG_CREATE_USER,
   USER_ID_NOT_FOUND,
   USER_NOT_FOUND,
   USER_ALREADY_EXISTS,
+  GOODBYE_MESSAGE,
 } = require('../utils/constants');
 
 const { jwtKey } = require('../utils/option');
 
 // Получение данных о пользователе
-module.exports.getUser = (req, res, next) => {
-  const userId = req.user._id;
+const getUser = (id, res, next) => {
   userSchema
-    .findById(userId)
-    .orFail(() => {
-      throw new NotFoundError(USER_ID_NOT_FOUND); // Пользователь с указанным _id не найден.
-    })
+    .findById(id)
+    .orFail()
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        next(new BadRequestError(USER_NOT_FOUND)); // Пользователь не найден.
-      } else {
-        next(err);
+      if (err.name === 'DocumentNotFoundError') {
+        // eslint-disable-next-line max-len
+        return next(new NotFoundError(USER_ID_NOT_FOUND)); // 'Пользователь с указанным _id не найден.
       }
+      return next(err);
     });
 };
+
+module.exports.getCurrentUser = (req, res, next) => getUser(req.user._id, res, next);
 
 // Создание пользователя
 module.exports.createUser = (req, res, next) => {
@@ -48,71 +49,94 @@ module.exports.createUser = (req, res, next) => {
   } = req.body;
 
   bcrypt.hash(password, 10)
-    .then((hash) => userSchema.create({
-      email,
-      password: hash,
-      name,
-    }))
-    .then((user) => res.status(201).send({
-      email: user.email,
-      name: user.name,
-      _id: user._id,
-    }))
-    .catch((err) => {
-      if (err.code === 11000) {
-        next(new ConflictError(EMAIL_ALREADY_EXISTS)); // Пользователь с таким email уже существует.
-      } else if (err.name === 'ValidationError') {
-        // eslint-disable-next-line max-len
-        next(new BadRequestError(WRONG_CREATE_USER)); // Переданы некорректные данные при создании пользователя.
-      } else {
-        next(err);
-      }
+    .then((hash) => {
+      userSchema.create({
+        email,
+        password: hash,
+        name,
+      })
+        .then((user) => {
+          const noValidUser = user.toObject({ useProjection: true });
+          return res.status(201).send(noValidUser);
+        })
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            // eslint-disable-next-line max-len
+            return next(new BadRequestError(WRONG_CREATE_USER)); // Пользователь с таким email уже существует.
+          }
+          if (err.code === 11000) {
+            // eslint-disable-next-line max-len
+            return next(new ConflictError(EMAIL_ALREADY_EXISTS)); // Переданы некорректные данные при создании пользователя.
+          }
+          return next(err);
+        });
     });
 };
 
 // Модификация пользователя
-module.exports.updateUser = (req, res, next) => {
-  const {
-    email,
-    name,
-  } = req.body;
-
+const updateUserData = (id, data, res, next) => {
   userSchema
     .findByIdAndUpdate(
-      req.user._id,
-      { email, name },
+      id,
+      data,
       {
         new: true,
         runValidators: true,
       },
     )
-    .orFail(() => {
-      throw new NotFoundError(USER_ID_NOT_FOUND); // Пользователь с указанным _id не найден.
-    })
+    .orFail()
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
       if (err.code === 11000) {
-        next(new ConflictError(USER_ALREADY_EXISTS)); // Такой пользователь уже существует.
-      } else if (err.name === 'ValidationError') {
-        // eslint-disable-next-line max-len
-        next(new BadRequestError(WRONG_UPDATE_PROFILE)); // Переданы некорректные данные при обновлении профиля.
-      } else {
-        next(err);
+        return next(new ConflictError(USER_ALREADY_EXISTS)); // Такой пользователь уже существует.
       }
+      if (err.name === 'DocumentNotFoundError') {
+        // eslint-disable-next-line max-len
+        return next(new NotFoundError(USER_NOT_FOUND)); // Пользователь не найден.
+      }
+      return next(err);
     });
+};
+
+module.exports.updateUser = (req, res, next) => {
+  const { name, email } = req.body;
+
+  return updateUserData(req.user._id, { name, email }, res, next);
 };
 
 // Логин пользователя
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  return userSchema
-    .findUserByCredentials(email, password)
+  userSchema.findOne({ email }).select('+password')
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : jwtKey, { expiresIn: '7d' });
-      res.send({ token });
+      if (!user) {
+        return next(new NotAuthError(WRONG_EMAIL_OR_PASSWORD));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return next(new NotAuthError(WRONG_EMAIL_OR_PASSWORD));
+          }
+
+          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : jwtKey, { expiresIn: '7d' });
+          // Добавлена авторизация через куки
+          res.cookie('jwt', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            saneSite: true,
+          });
+
+          return res.send(user.toJSON({ useProjection: true }));
+        });
     })
     .catch(next);
+};
+
+// Деавторизация пользователя
+module.exports.logout = (req, res) => {
+  res.clearCookie('jwt').send({ message: GOODBYE_MESSAGE });
 };
