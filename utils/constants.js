a3345e9b775fdae2c47595fd91cfda26c2c8f764
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-07-14 16:46:55 +0300

    Добавлена cors-политика и cookie-parsel

--- utils/constants.js
+++ utils/constants.js
@@ -5,11 +5,11 @@ const WRONG_EMAIL = 'Неправильный формат почты.';
 const WRONG_URL = 'Некорректная ссылка.';
 const URL_NOT_FOUND = '404: Ошибка! Данные не найдены!';
 const WRONG_DATA = 'Переданы некорректные данные.';
+const GOODBYE_MESSAGE = 'До свидания!';
 
 // Блок пользователя
 const EMAIL_ALREADY_EXISTS = 'Пользователь с таким email уже существует.';
 const WRONG_CREATE_USER = 'Переданы некорректные данные при создании пользователя.';
-const WRONG_UPDATE_PROFILE = 'Переданы некорректные данные при обновлении профиля.';
 const USER_ID_NOT_FOUND = 'Пользователь с указанным _id не найден.';
 const USER_NOT_FOUND = 'Пользователь не найден.';
 const USER_ALREADY_EXISTS = 'Такой пользователь уже существует.';
@@ -17,13 +17,27 @@ const USER_ALREADY_EXISTS = 'Такой пользователь уже суще
 // Блок с фильмами
 const ACCESS_ERROR = 'Отказано в доступе! Данный фильм не принадлежит пользователю!';
 const NOT_FOUND_MOVIE = 'Фильм с указанным _id не найден.';
-const WRONG_MOVIE = 'Переданы некорректные данные при создании фильма.';
 const DELETE_WRONG_MOVIE = 'Переданы некорректные данные при удалении!';
+
+// Корс политика
+const allowedCors = [
+  'https://rocketsaladgirl.nomoredomains.rocks',
+  'http://rocketsaladgirl.nomoredomains.rocks',
+  'localhost:3000',
+  'http://localhost',
+  'http://localhost:3001',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: allowedCors,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
 module.exports = {
   EMAIL_ALREADY_EXISTS,
   WRONG_CREATE_USER,
-  WRONG_UPDATE_PROFILE,
   USER_NOT_FOUND,
   USER_ID_NOT_FOUND,
   USER_ALREADY_EXISTS,
@@ -37,5 +51,7 @@ module.exports = {
   DELETE_WRONG_MOVIE,
   ACCESS_ERROR,
   NOT_FOUND_MOVIE,
-  WRONG_MOVIE,
+  GOODBYE_MESSAGE,
+  allowedCors,
+  corsOptions,
 };
